2b7762d8a44d26f54206545a1ce477ab139dc019
aleeeee1 <alexfapper.2005@gmail.com>
2022-09-26 06:17:18 +0200

    minor fix

--- src/Register.js
+++ src/Register.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { SpotifyHandler } from "./api/SpotifyHandler";
-import {creation} from "./CreationHandler";
+import { creation } from "./CreationHandler";
 import Cookies from "js-cookie";
 
 // await function GetGenders(){
@@ -9,73 +9,52 @@ import Cookies from "js-cookie";
 //     this.setState({ totalReactPackages: data.total })
 // }
 
-
 function Register() {
-    const [genders, setGenders] = useState([]);
+  const [genders, setGenders] = useState([]);
 
+  // add error handler: https://reactjs.org/docs/faq-ajax.html
+  useEffect(() => {
+    fetch("/api/v1/genders")
+      .then((res) => res.json())
+      .then((result) => {
+        setGenders(result);
+      });
 
-    // add error handler: https://reactjs.org/docs/faq-ajax.html
-    useEffect(() => {
-        fetch("/api/v1/genders")
-            .then(res => res.json())
-            .then(
-            (result) => {
-                setGenders(result);
-            },)
-            
-        
-        creation();
-        
-      },[]);
+    creation();
+  }, []);
 
-   
-
-    console.log(genders);
-    
-
+  console.log(genders);
 
   return (
-    
     // the LoginPage is set to have the same background as the original login page
     <div className="Register LoginPage">
-
-    
-        <div className="RegistrationForm">
-            <div className="RegistrationHeader">
-                <h1>Registration</h1>
-            </div>
-            <div className="InfoContainer">
-                <div className="NickName">
-
-                
-
-                </div>
-                <div className="photo"></div>
-            </div>
-            <div className="UserGenre">
-
-                <select className="Selector">
-                    {/* per ogni elemento nella lista, si fa un'opzione da selezionare */}
-                    {genders.map(gender => (
-                        <option key={gender.id} value={gender.name}>
-                            {gender.name}</option>
-                    ))}
-
-                   
-                </select>
-
-            </div>
-            <div className="Bio">
-                
-            </div>
-            <div className="Instagram"></div>
+      <div className="RegistrationForm">
+        <div className="RegistrationHeader">
+          <h1>Registration</h1>
         </div>
+        <div className="InfoContainer">
+          <div className="NickName"></div>
+          <div className="photo"></div>
+        </div>
+        <div className="UserGenre">
+          <select className="Selector">
+            {/* per ogni elemento nella lista, si fa un'opzione da selezionare */}
+            {genders.length > 0 ? (
+              genders.map((gender) => (
+                <option key={gender.id} value={gender.name}>
+                  {gender.name}
+                </option>
+              ))
+            ) : (
+              <option>Il server è down</option>
+            )}
+          </select>
+        </div>
+        <div className="Bio"></div>
+        <div className="Instagram"></div>
+      </div>
     </div>
   );
 }
-
-
-
-
 
 export default Register;
