0fc68220a034c02d1fea4e0abf773051958eb3e0
Zubby <mrgabrielborgo@gmail.com>
2022-09-26 20:09:19 +0200

    registration page continued

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Home from "./Home";
 import Test from "./Test";
 import Login from "./Login";
 import Register from "./Register";
+import Error from "./Error";
 
 import {
   BrowserRouter as Router,
@@ -26,6 +27,7 @@ function App() {
           <Route path="/test" element={<Test />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/error" element={<Error />} />
           <Route exact path="/" element={<Navigate to="/login" replace />} />
         </Routes>
       </Router>
---
+++ src/Error.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+
+
+function Error() {
+  
+
+  return (
+    <div>
+      <h1>ERROR</h1>
+    </div>
+  );
+}
+
+export default Error;
