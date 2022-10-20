93cf327a1a1b01043251aed78a0017a6ec7bdcfe
baka <baka@BigMac.homenet.telecomitalia.it>
2022-10-20 18:40:22 +0200

    sync

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Card.css";
+
+function Card(props) {
+  console.log(props.obj);
+  return (
+    <div className="Card">
+      <h1 className="User-Name">{props.obj.user.username}</h1>
+    </div>
+  );
+}
+
+export default Card;
